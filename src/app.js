36b6026f83df59b11dc499afc9ab181c6fcc7687
Corbin <corbindanger7@gmail.com>
2020-08-30 17:54:32 -0700

    grid

--- src/app.js
+++ src/app.js
@@ -2,13 +2,44 @@ import React from 'react';
 import './app.css';
 import Pixel from './pixel';
 
-function App() {
-  return (
-    <div className="App">
-      CORBIN IS COOL TRAVIS IS NOT
-      <Pixel />
-    </div>
-  );
+class App extends React.Component {
+
+  rows = 5;
+  cols = 5;
+
+  handlePixelClick() {
+    alert('a pixel was clicked!');
+  }
+
+  renderPixels() {
+    var pixels = [];
+
+    for (let i = 0; i < this.rows * this.cols; i++) {
+      pixels.push(
+        <Pixel
+          onClick={() => this.handlePixelClick()}
+        />
+      );
+    }
+    
+    console.log(pixels);
+    return pixels;
+  }
+
+  render() {
+    var pixels = this.renderPixels();
+
+    return (
+      <div className="app">
+        CORBIN IS COOL TRAVIS IS NOT
+        <div className="grid">
+        {pixels}
+        </div>
+        
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/pixel.js
+++ src/pixel.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class Pixel extends React.Component {
-    render() {
-      return (
-        <div className="pixel">
-            pixel
-        </div>
-      );
+    constructor(props) {
+        super(props);
+
     }
-  }
-  
-  export default Pixel
+
+    render() {
+        return (
+            <div className="pixel"
+                onClick={() => this.props.onClick()}
+            >
+
+            </div>
+        );
+    }
+}
+
+export default Pixel
